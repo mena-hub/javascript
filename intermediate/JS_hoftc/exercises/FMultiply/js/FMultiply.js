bbe91df8011a87fd116233ec07811944a3ecf1fa
mena-hub <mssz.nnia@gmail.com>
2021-03-13 14:13:50 -0300

    specialMultiply

---
+++ intermediate/JS_hoftc/exercises/FMultiply/js/FMultiply.js
@@ -0,0 +1,12 @@
+function specialMultiply(a, b) {
+    if (arguments.length === 1) {
+        return function(b) {
+            return a * b;
+        };
+    }
+    return a * b;
+}
+
+console.log(specialMultiply(3, 4));
+console.log(specialMultiply(3)(4));
+console.log(specialMultiply(3));
